237455966b6b7436b9197a0af994e310dafaf0aa
GotouTamaki <134473633+GotouTamaki@users.noreply.github.com>
2023-06-20 14:57:26 +0900

    レーザーポインターの下準備をしました。

--- Assets/Scripts/LaserPointerController.cs
+++ Assets/Scripts/LaserPointerController.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class LaserPointerController : MonoBehaviour
 {
+    [SerializeField] 
+    GameObject _crossehair;
+    // –C’e‚Æ‚µ‚Ä¶¬‚·‚éƒvƒŒƒnƒu
+    [SerializeField]
+    GameObject _shellPrefab = default;
+
+    AudioSource _audio = default;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _audio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        this.transform.up = _crossehair.transform.position - transform.position;
     }
 }
