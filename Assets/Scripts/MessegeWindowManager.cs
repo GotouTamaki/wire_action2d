d46eb14fd37a496bee45f175df9f98831ec5c8eb
GotouTamaki <134473633+GotouTamaki@users.noreply.github.com>
2023-06-23 19:16:32 +0900

    メッセージウィンドウのテンプレートを制作しました。

--- Assets/Scripts/MessegeWindowManager.cs
+++ Assets/Scripts/MessegeWindowManager.cs
@@ -1,25 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MessegeWindowManager : MonoBehaviour
 {
-    [SerializeField]
-    private Transform _targetTfm;
-    [SerializeField]
-    private Vector3 _offset = new Vector3(0, 1.5f, 0);
-
-    private RectTransform _myRectTfm;
+    [SerializeField] Text _messegeWindow = null;
 
     // Start is called before the first frame update
     void Start()
     {
-        _myRectTfm = GetComponent<RectTransform>();
+        _messegeWindow.enabled = false;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        _myRectTfm.position = RectTransformUtility.WorldToScreenPoint(Camera.main, _targetTfm.position + _offset);
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Target"))
+        {
+            _messegeWindow.enabled = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Target"))
+        {
+            _messegeWindow.enabled = false;
+        }
     }
 }
