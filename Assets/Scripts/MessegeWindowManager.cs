15908ac5c704965e1c9385609ea2d7a3a431249e
GotouTamaki <134473633+GotouTamaki@users.noreply.github.com>
2023-06-30 23:10:51 +0900

    チュートリアルステージの制作を進めました。

--- Assets/Scripts/MessegeWindowManager.cs
+++ Assets/Scripts/MessegeWindowManager.cs
@@ -6,6 +6,10 @@ using UnityEngine.UI;
 public class MessegeWindowManager : MonoBehaviour
 {
     [SerializeField] Text _messegeWindow = null;
+    [SerializeField] GameObject _target = null;
+    [SerializeField] float _onMessegeDistance = 1;
+
+    float distance = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -16,22 +20,31 @@ public class MessegeWindowManager : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        
-    }
+        distance = Vector2.Distance(this.transform.position, _target.transform.position);
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject.CompareTag("Target"))
+        if (distance < _onMessegeDistance) 
         {
             _messegeWindow.enabled = true;
         }
-    }
-
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.gameObject.CompareTag("Target"))
+        else
         {
             _messegeWindow.enabled = false;
         }
     }
+
+    //private void OnTriggerEnter2D(Collider2D other)
+    //{
+    //    if (other.gameObject.CompareTag("Target"))
+    //    {
+    //        _messegeWindow.enabled = true;
+    //    }
+    //}
+
+    //private void OnTriggerExit2D(Collider2D other)
+    //{
+    //    if (other.gameObject.CompareTag("Target"))
+    //    {
+    //        _messegeWindow.enabled = false;
+    //    }
+    //}
 }
